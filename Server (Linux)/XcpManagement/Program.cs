66f9e4d61f628b4ea760cf65d25bad0529373763
franklethemouse <franklethemouse@gmail.com>
2026-02-01 13:16:04 +1100

    updates

--- Server (Linux)/XcpManagement/Program.cs
+++ Server (Linux)/XcpManagement/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using XcpManagement.Data;
